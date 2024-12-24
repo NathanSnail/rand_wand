db6f4315b64a2cc46e89645e67e1fffefc7c455b
Nathan <nathan.titirangi@gmail.com>
2024-12-24 22:58:47 +1300

    better usage

--- main.lua
+++ main.lua
@@ -3,37 +3,45 @@ local socket = require("socket")
 ---@class ExprLike
 ---@field sample fun(self: ExprLike): string[]
 ---@field id string
----@operator call(number): UsableExpr
+---@operator call(number): WeightedExpr
 ---@operator mul(ExprLike): Prod
 
 ---@class Unit: ExprLike
 ---@field id "Unit"
 ---@field spell string
----@operator call(number): UsableExpr
+---@operator call(number): WeightedExpr
 ---@operator mul(ExprLike): Prod
 
 ---@class Sum: ExprLike
 ---@field id "Sum"
----@field children UsableExpr[]
----@operator call(number): UsableExpr
----@operator add(UsableExpr | Sum): Sum
+---@field children WeightedExpr[]
+---@operator call(number): WeightedExpr
+---@operator add(WeightedExpr | Sum): Sum
 ---@operator mul(ExprLike): Prod
 
 ---@class Prod: ExprLike
 ---@field id "Prod"
 ---@field children ExprLike[]
----@operator call(number): UsableExpr
+---@operator call(number): WeightedExpr
 ---@operator mul(ExprLike): Prod
 
 ---@class Maybe: ExprLike
 ---@field id "Maybe"
----@field element UsableExpr
----@operator call(number): UsableExpr
+---@field element WeightedExpr
+---@operator call(number): WeightedExpr
 ---@operator mul(ExprLike): Prod
 
----@class UsableExpr: ExprLike
+---@class Indirect: ExprLike
+---@field id "Indirect"
+---@field ref ExprLike?
+---@operator call(number): WeightedExpr
+---@operator mul(ExprLike): Prod
+
+---@class WeightedExpr: ExprLike
+---@field id "Weighted"
+---@field expr ExprLike
 ---@field weight number
----@operator add(UsableExpr | Sum): Sum
+---@operator add(WeightedExpr | Sum): Sum
 ---@operator unm(): Maybe
 
 ---@generic T
@@ -41,6 +49,9 @@ local socket = require("socket")
 ---@return T
 local function copy(t)
 	if type(t) ~= "table" then
+		return t
+	end
+	if t.id == "Indirect" then
 		return t
 	end
 	local o = {}
@@ -71,8 +82,8 @@ local function mergerFunction(self, other, key)
 	return setmetatable({ id = key, children = { self, other } }, expr_mt)
 end
 
----@param self UsableExpr
----@param other UsableExpr
+---@param self WeightedExpr
+---@param other WeightedExpr
 ---@return Sum
 local function sum(self, other)
 	return mergerFunction(self, other, "Sum")
@@ -87,12 +98,9 @@ end
 
 ---@param self ExprLike
 ---@param weight number
----@return UsableExpr
+---@return WeightedExpr
 local function call(self, weight)
-	local modifiable = copy(self)
-	---@cast modifiable UsableExpr
-	modifiable.weight = weight
-	return modifiable
+	return setmetatable({ expr = self, weight = weight, id = "Weighted" }, expr_mt)
 end
 
 ---@param id string
@@ -101,10 +109,15 @@ local function unit(id)
 	return setmetatable({ id = "Unit", spell = id }, expr_mt)
 end
 
----@param self UsableExpr
+---@param self WeightedExpr
 ---@return Maybe
 local function maybe(self)
 	return setmetatable({ id = "Maybe", element = self }, expr_mt)
+end
+
+---@return Indirect
+local function indirect()
+	return setmetatable({ id = "Indirect" }, expr_mt)
 end
 
 expr_mt = {
@@ -123,7 +136,7 @@ expr_mt = {
 					sigma = sigma + v.weight
 				end
 				local sample = math.random() * sigma
-				---@type UsableExpr?
+				---@type WeightedExpr?
 				local choice
 				local cumulative = 0
 				for _, v in ipairs(self.children) do
@@ -157,6 +170,15 @@ expr_mt = {
 					return self.element:sample()
 				end
 				return {}
+			elseif self.id == "Indirect" then
+				---@cast self Indirect
+				if self.ref == nil then
+					error("indirect value required but it doesn't exist yet")
+				end
+				return self.ref:sample()
+			elseif self.id == "Weighted" then
+				---@cast self WeightedExpr
+				return self.expr:sample()
 			else
 				error("invalid instance of expr " .. self.id)
 			end
@@ -174,13 +196,15 @@ end
 
 math.randomseed(socket.gettime())
 local chainsaw = unit("CHAINSAW")
+
 local spark = unit("LIGHT_BULLET")
 local sparkt = unit("LIGHT_BULLET_TRIGGER")
 local proj = chainsaw(1) + spark(1) + sparkt(1)
-sample(proj)
+-- sample(proj)
 local maybe_chainsaw = -chainsaw(0.5)
 local probably_chainsaw = maybe_chainsaw(0.5) + chainsaw(0.5)
 local might_be_2_chainsaws = chainsaw * probably_chainsaw
-for _, v in ipairs(might_be_2_chainsaws:sample()) do
-	print(v)
-end
+-- sample(might_be_2_chainsaws)
+local spam_wand = indirect()
+spam_wand.ref = chainsaw(1) + (proj * spam_wand)(5)
+sample(spam_wand)
